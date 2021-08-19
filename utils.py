21e626cb323964088fe4be40748c2ba0680fa0df
Panchajanya1999 <panchajanya@azure-dev.live>
2021-08-19 10:33:34 +0530

    utils: Downgrade binutils version 2.38 > 2.31
    Most of the distros are using GLIBC 2.31 so users trying to compile
    a kernel are getting glibc errors.
    Downgrade the binutils to fix the error as of now.
    
    Change-Id: I8961554c0ffbb4a7d39c7f432364f87f0b1e9a49
    Signed-off-by: Panchajanya1999 <panchajanya@azure-dev.live>
    Signed-off-by: vijaymalav564 <jaymalav10@gmail.com>

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def current_binutils():
     Simple getter for current stable binutils release
     :return: The current stable release of binutils
     """
-    return "binutils-2.38"
+    return "binutils-2.31"
 
 
 def download_binutils(folder):
