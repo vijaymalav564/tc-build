f26a7f277b5e07c500b98f5d27f7d03d950df7dc
vijaymalav564 <jaymalav10@gmail.com>
2021-12-24 03:24:03 +0000

    Revert "utils: Don't verify binutils tarballs checksum"
    This reverts commit 42f769543ef7f8ec829cc0e89aa7a34b78118f05.

--- utils.py
+++ utils.py
@@ -48,6 +48,7 @@ def download_binutils(folder):
             "https://ftp.gnu.org/gnu/binutils/" + binutils_tarball.name
         ],
                        check=True)
+        verify_binutils_checksum(binutils_tarball)
         # Extract the tarball then remove it
         subprocess.run(["tar", "-xJf", binutils_tarball.name],
                        check=True,
