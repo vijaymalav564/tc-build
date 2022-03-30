d95c2585537c068389cd7579be0da26d6b279a8e
vijaymalav564 <jaymalav10@gmail.com>
2022-03-30 05:37:36 +0000

    utils: upgrade binutils version 2.31 < 2.33.1
    Signed-off-by: vijaymalav564 <jaymalav10@gmail.com>

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def current_binutils():
     Simple getter for current stable binutils release
     :return: The current stable release of binutils
     """
-    return "binutils-2.31"
+    return "binutils-2.33.1"
 
 
 def download_binutils(folder):
